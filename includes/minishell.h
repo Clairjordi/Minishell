fd55ecc24f64cba7a14b1c39b4f87ca6c663e575
Claire Lorcery <clorcery@1A1.42angouleme.fr>
2022-09-05 18:59:06 +0200

    ctrl D ok

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: clorcery <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 12:33:49 by clorcery          #+#    #+#             */
-/*   Updated: 2022/09/05 14:00:04 by clorcery         ###   ########.fr       */
+/*   Updated: 2022/09/05 18:55:07 by clorcery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 typedef struct s_minishell
 {
--- mandatory/main.c
+++ mandatory/main.c
@@ -6,57 +6,36 @@
 /*   By: clorcery <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 12:43:25 by clorcery          #+#    #+#             */
-/*   Updated: 2022/09/05 15:03:06 by clorcery         ###   ########.fr       */
+/*   Updated: 2022/09/05 18:55:55 by clorcery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	handler(int sig, t_shell shell)
-{
-	if (sig == SIGINT)
-	{
-		ft_free();
-		exit(0);
-	}
-}
-/* void	handler(int sig, t_shell shell) */
-/* { */
-/* 	if (sig == SIGINT) */
-/* 	{ */
-/* 		ft_free(); */
-/* 		exit(0); */
-/* 	} */
-/* } */
-
-int	main(int argc, char *argv[])
+int	main(void)
 {
 	t_shell	shell;
-	char	*end;
+	pid_t	pid;
 
-
-	(void) argc;
-	(void) argv;
-	end = "fin\n";
-	signal(SIGINT, handler);
-//	signal(SIGINT, handler);
-	while (1)
-	{
-		write(1, "$>", 2);
-		shell.str = get_next_line(0);
-		/* str = readline ("$>"); */
-		/* if (!str) */
-		/* { */
-		/* 	free(str); */
-		/* 	exit (1); */
-		/* } */
-		/* add_history(str); */
-		if (!ft_strcmp(shell.str, end))
+	pid = fork();
+	if (pid == -1)
+		exit(1);
+	else if (pid == 0)
+	{	
+		while (1)
 		{
+			shell.str = readline ("$>");
+			if (!shell.str)
+			{
+				ft_putstr_fd("exit", 1);
+				exit (1);
+			}
+			if (shell.str)
+				add_history(shell.str);
 			free(shell.str);
-			exit(0);
 		}
-		free(shell.str);
 	}
+	else
+		waitpid(pid, NULL, 0);
 	return (0);
 }
